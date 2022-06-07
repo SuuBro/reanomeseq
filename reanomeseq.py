a4e8819d4a2f3ad0012edfb005c7ae2ba1e56d09
SuuBro <J.Subra@gmail.com>
2022-06-07 22:28:43 +0100

    Refactor

--- reanomeseq.py
+++ reanomeseq.py
@@ -5,15 +5,16 @@
 import asyncio
 import monome
 import reapy
+import numpy as np
 
 from reapy import reascript_api as RPR
 from typing import List, Tuple
 
 from arc import Arc
-from grid import Grid
 
 class Note():
-    def __init__(self, start: int, end: int, pitch: int, velocity: int, channel: int):
+    def __init__(self, index: int, start: int, end: int, pitch: int, velocity: int, channel: int):
+        self.index = index
         self.start = start
         self.end = end
         self.pitch = pitch
@@ -23,68 +24,98 @@ class Note():
     def to_string(self):
         return f'{self.start:10.4f} -> {self.end:10.4f}: {self.pitch}'
 
-class GridView:
-    def __init__(self, grid: Grid):
-        self.grid = grid
+class VaribrightGrid(monome.Grid):
+    def __init__(self):
+        super().__init__()
+
+    def _set_varibright(self):
+        self.varibright = True
+
+
+class GridApp(monome.GridApp):
+    def __init__(self):
+        super().__init__(VaribrightGrid())
         self.zoom = 2
         self.earliest_displayed_time = 0
         self.lowest_displayed_note = 48
         self.note_scale = None
+        self.note_lookup = np.full((16,8), -1, dtype=int)
+
+    def on_grid_ready(self):
+        print('Grid ready.')
+        self.grid.led_all(0)
+        asyncio.create_task(self.reaper_loop())
+
+
+    def on_grid_disconnect(self):
+        print('Grid disconnected.')
+
+
+    def draw_note(self, start: int, end: int, row: int):
+        self.grid.led_level_set(start, 7-row, 15)
+        for x in range(start+1, end):
+            self.grid.led_level_set(x, 7-row, 4)
+
+
+    def on_grid_key(self, x: int, y: int, s: int):
+        print(f'on_grid_key {x} {y} {s}')
+
 
     def render_notes(self, bpm: float, notes: List[Note]):
-        self.grid.clear()
+        self.grid.led_all(0)
+        self.note_lookup = np.full((16,8), -1, dtype=int)
+
         for note in notes:
-            note_start_col = int(note.start / (bpm * self.zoom))
-            note_end_col = int(note.end / (bpm * self.zoom))
-
-            if(note_end_col < 0):
-                continue # before display
-            if(note_start_col > 15):
-                continue # after display
-
+            note_start_col = max(int(note.start / (bpm * self.zoom)), 0)
+            note_end_col = min(int(note.end / (bpm * self.zoom)), 15)
             note_row = note.pitch - self.lowest_displayed_note
-            self.grid.draw_note(note_start_col, note_end_col, note_row)
+
+            if note_end_col < 0 or note_start_col > 15 or note_row < 0 or note_row > 7:
+                continue # before display
+
+            for x in range(note_start_col,note_end_col):
+                self.note_lookup[x,note_row] = note.index
+
+            self.draw_note(note_start_col, note_end_col, note_row)
 
 
-@reapy.inside_reaper()
-def get_notes(track: any) -> Tuple[int, List[Note]]:
-    media_item = RPR.GetTrackMediaItem(track, 0)
-    project = RPR.GetItemProjectContext(media_item)
-    _, bpm, _ = RPR.GetProjectTimeSignature2(project, 0, 0)
-    take = RPR.GetTake(media_item, 0)
-    _, _, n, _, _  = RPR.MIDI_CountEvts(take, 0, 0, 0)
+    async def reaper_loop(self):
+        previous_hash = ""
 
-    notes = []
-    for i in range(0, n):
-        _, take, i, _, _, start, end, chan, pitch, vel = RPR.MIDI_GetNote(take, i, False, False, 0, 1, 0, 0, 0)
-        notes.append(Note(start, end, pitch, vel, chan))
+        while True:
+            track = RPR.GetSelectedTrack(0, 0)
+            _, _, _, hash, _ = RPR.MIDI_GetTrackHash(track, True, "", 100)
 
-    return bpm, notes
+            if hash == previous_hash:
+                await asyncio.sleep(0.1)
+                continue
+
+            bpm, notes = self.get_notes(track)
+            self.render_notes(bpm, notes)
+
+            previous_hash = hash
+            await asyncio.sleep(0.05)
 
 
-async def reaper_loop(gridView: GridView):
-    previous_hash = ""
+    @reapy.inside_reaper()
+    def get_notes(self, track: any) -> Tuple[int, List[Note]]:
+        media_item = RPR.GetTrackMediaItem(track, 0)
+        project = RPR.GetItemProjectContext(media_item)
+        _, bpm, _ = RPR.GetProjectTimeSignature2(project, 0, 0)
+        take = RPR.GetTake(media_item, 0)
+        _, _, n, _, _  = RPR.MIDI_CountEvts(take, 0, 0, 0)
 
-    while True:
-        track = RPR.GetSelectedTrack(0, 0)
-        _, _, _, hash, hs = RPR.MIDI_GetTrackHash(track, True, "", 100)
+        notes = []
+        for i in range(0, n):
+            _, take, i, _, _, start, end, chan, pitch, vel = RPR.MIDI_GetNote(take, i, False, False, 0, 1, 0, 0, 0)
+            notes.append(Note(i, start, end, pitch, vel, chan))
 
-        if hash == previous_hash:
-            await asyncio.sleep(0.1)
-            continue
-
-        bpm, notes = get_notes(track)
-        gridView.render_notes(bpm, notes)
-
-        previous_hash = hash
-        await asyncio.sleep(0.05)
-
+        return bpm, notes
 
 async def main():
     loop = asyncio.get_running_loop()
     arc = Arc()
-    grid = Grid()
-    gridView = GridView(grid)
+    grid = GridApp()
 
     def serialosc_device_added(id, type, port):
         print(f'connecting to {id} ({type}) on {port}')
@@ -95,8 +126,6 @@ async def main():
 
     serialosc = monome.SerialOsc()
     serialosc.device_added_event.add_handler(serialosc_device_added)
-
-    asyncio.create_task(reaper_loop(gridView))
 
     await serialosc.connect()
     await loop.create_future()
